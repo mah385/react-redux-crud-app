c45e3836f34b4d666df90cac9edb242db13914df
mah385 <mdarifhussain385@gmail.com>
2020-03-21 15:04:42 +0530

    added Redux support for other browsers than Chrome

--- src/store.js
+++ src/store.js
@@ -1,14 +1,21 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 
 // use applyMiddleware to add the thunk middleware to the store
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+let store;
+
+if (window.navigator.userAgent.includes("Chrome")) {
+  store = createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+} else {
+  store = createStore(rootReducer, compose(applyMiddleware(thunk)));
+}
 
 export default store;
